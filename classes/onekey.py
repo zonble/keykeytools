7b0457d7dfb1fdded58ebcb2bdc5e7ee34389e0c
zonble <zonble@gmail.com>
2009-04-27 03:30:45 +0800

    Completes the help text for the onekey setting generator.

--- classes/onekey.py
+++ classes/onekey.py
@@ -122,7 +122,7 @@ class MainHandler(webapp.RequestHandler):
 		pass
 	def post(self):
 		self.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
-		self.response.headers['Content-Disposition'] = 'attachment; filename=OneKey.plist'
+		self.response.headers['Content-Disposition'] = 'attachment; filename=UserOneKey.plist'
 		header = """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
