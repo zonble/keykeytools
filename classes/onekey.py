d434de45f7df8505bfcf13b17ce9071e850bcedc
zonble <zonble@gmail.com>
2009-04-27 02:34:37 +0800

    Removes the jquery files. Updates the code of the onekey setting generator.

--- classes/onekey.py
+++ classes/onekey.py
@@ -5,6 +5,7 @@
 import os
 import sys
 import wsgiref.handlers
+import urllib
 from google.appengine.ext import webapp
 from google.appengine.ext import db
 from google.appengine.api import users
@@ -33,7 +34,10 @@ class ItemList():
 		html = u"<ul>"
 		for item in items:
 			title = item.title
-			item_html = u'<li class="draggable">' + title + u'</li>\n'
+			item_id = item.key().id()
+			item_html = u'<li class="draggable"><a class="remove_link" href="#">[x]</a> ' + title
+			item_html = item_html + '<input type="hidden" name="selected_items[]" value="' + str(item_id) + '" checked="checked" />'
+			item_html = item_html + u'</li>\n'
 			html = html + item_html
 		html = html + u"</ul>"
 		return html
@@ -90,6 +94,52 @@ class MainHandler(webapp.RequestHandler):
 		self.response.out.write(template.render(path, template_values))
 		self.response.out.write(html.sharedHTML.toolbar())
 		self.response.out.write(html.sharedHTML.footer())
+	def xml_for_key_and_value(self, key, value):
+		xml = u"\t\t\t\t<key>" + key + "</key>\n"
+		xml = xml + u"\t\t\t\t<string>" + value + "</string>\n"
+		return xml
+	def xml_for_item_id(self, item_id):
+		xml = u"\t\t\t<dict>\n"
+		item = OneKeyItem.get_by_id(item_id)
+		service_name = item.service_name
+		xml = xml + self.xml_for_key_and_value("ServiceName", service_name)
+		title = item.title
+		xml = xml + self.xml_for_key_and_value("Title", title)
+		action = item.action
+		xml = xml + self.xml_for_key_and_value("Action", action)
+		action_url = item.action_url
+		action_url = action_url.replace("&", "&amp;")
+		# action_url = urllib.quote_plus(action_url)
+		xml = xml + self.xml_for_key_and_value("ActionURL", action_url)		
+		place_holder_encoding = item.place_holder_encoding
+		xml = xml + self.xml_for_key_and_value("PlaceHolderEncoding", place_holder_encoding)
+		input_prompt = item.input_prompt
+		xml = xml + self.xml_for_key_and_value("InputPrompt", input_prompt)
+		input_prompt_description = item.input_prompt_description
+		xml = xml + self.xml_for_key_and_value("InputPromptDescription", input_prompt_description)
+		xml = xml + u"\t\t\t</dict>\n"
+		return xml
+		pass
+	def post(self):
+		self.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
+		self.response.headers['Content-Disposition'] = 'attachment; filename=OneKey.plist'
+		header = """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+	<dict>
+		<key>Features</key>
+		<array>
+"""
+		self.response.out.write(header)
+		selected_items = self.request.get_all("selected_items[]")
+		for item_id in selected_items:
+			xml = self.xml_for_item_id(int(item_id))
+			self.response.out.write(xml)
+		footer = """		</array>
+	</dict>
+</plist>"""
+		self.response.out.write(footer)
+		pass
 
 def main():
 	application = webapp.WSGIApplication([
