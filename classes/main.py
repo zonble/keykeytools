a923488107486f30d729db00fd02260217316840
zonble <zonble@gmail.com>
2009-04-29 14:00:52 +0800

    Adds a page for the input methods removal tool.

--- classes/main.py
+++ classes/main.py
@@ -20,9 +20,21 @@ class MainHandler(webapp.RequestHandler):
 		self.response.out.write(html.sharedHTML.header())
 		self.response.out.write(template.render(path, template_values))
 		self.response.out.write(html.sharedHTML.footer())
+		
+class IMRemovalHandler(webapp.RequestHandler):
+	def get(self):
+		template_values = {}
+		path = os.path.join(os.path.dirname(__file__), '..', 'html', 'imremoval.html')
+		self.response.out.write(html.sharedHTML.header(u"Mac OS X 輸入法移除工具"))
+		self.response.out.write(template.render(path, template_values))
+		self.response.out.write(html.sharedHTML.toolbar())
+		self.response.out.write(html.sharedHTML.footer())
 
 def main():
-	application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
+	application = webapp.WSGIApplication([
+		('/', MainHandler),
+		('/imremoval', IMRemovalHandler),
+		], debug=True)
 	wsgiref.handlers.CGIHandler().run(application)
 
 if __name__ == '__main__':
